af7709dcf0f7dca365fabfe488f87ba8b61f6d04
MThien <minhthiennguyen1997@gmail.com>
2019-11-17 06:33:38 +0700

    update bus 18

--- JS/busInfo18.js
+++ JS/busInfo18.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var data;
     $.ajax({
         dataType: "json",
-        url: 'http://localhost:8899/businfo/findById/24',
+        url: 'http://localhost:8899/businfo/findById/18',
         data: data,
         error: function() {
             $('#info').html('<p>Lỗi Khi đọc dữ liệu</p>');
@@ -29,25 +29,6 @@ $(document).ready(function() {
             $('#runTime').text(data.runTime);
             $('#routeLength').text(data.routeLength);
 
-        }
-    });
-});
-$(document).ready(function() {
-    var data;
-    $.ajax({
-        dataType: "json",
-        url: 'http://localhost:8899/businfo/getBusInfoAll',
-        data: data,
-        error: function() {
-            $('#infoBusList').html('<p>Lỗi Khi đọc dữ liệu</p>');
-        },
-        success: function(data) {
-            for (let index = 0; index < data.length; index++) {
-                const id = data[index].id;
-                const name = data[index].name;
-                var $busStationName = "<li class='stop-container' role='stopContainer' tabindex='-1'><div class='stop-wrapper '><h3>" + id + " : " + name + "</h3></div></li>";
-                $('#infoBusList').append($busStationName);
-            }
         }
     });
 });
--- JS/index.js
+++ JS/index.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#timTuyenXe").autocomplete({
         source: [
-            { value: "07 : Chợ Lớn‎ → Bãi Hậu Cần Số 1", url: "so7.html" },
-            { value: "18 : Sài Gòn ‎→ Hiệp Thành", url: "so18.html" },
-            { value: "24 : 19/5 ‎→ Bến Xe Miền Đông", url: "so24.html" },
-            { value: "148 : Cần Số 1‎ → Bến Xe Miền Tây", url: "so148.html" }
+            { value: "07 : Chợ Lớn‎ → Gò vấp", url: "so7.html" },
+            { value: "18 : Bến Thành ‎→ Hiệp Thành", url: "so18.html" },
+            { value: "24 : Bến Xe Miền Đông ‎→ Hóc Môn", url: "so24.html" },
+            { value: "148 : Bến Xe Miền Tây → Gò Vấp", url: "so148.html" }
         ],
         select: function(event, ui) {
             window.location = ui.item.url;
